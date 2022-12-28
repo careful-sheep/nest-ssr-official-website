00bbb5e12900441ebd02e419eebafa59c082f3bc
careful-sheep <1092979868@qq.com>
2022-12-28 16:12:28 +0800

    feat: chrome支持服务端调试

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import styles from '../styles/Home.module.scss';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+    debugger;
     return (
         <>
             <Head>
